3891dadedbbf6a6763b1aeab9af3553f85390060
matheuscamilo <matheuscmilo@gmail.com>
2023-02-28 23:03:24 +0100

    update clustering

--- tpot/config/clustering.py
+++ tpot/config/clustering.py
@@ -31,31 +31,30 @@ clustering_config_dict = {
 
     # Clusterers
     
-    # 'sklearn.cluster.AgglomerativeClustering': {
-    #     'n_clusters': range(2, 21),
-    #     'metric': ['euclidean', 'l1', 'l2', 'manhattan', 'cosine', 'precomputed', 'cityblock'],
-    #     'linkage': ['ward', 'complete', 'average', 'single'],
-    # },
+    'sklearn.cluster.AgglomerativeClustering': {
+        'n_clusters': range(2, 21),
+        'metric': ['euclidean', 'l1', 'l2', 'manhattan', 'cosine', 'precomputed', 'cityblock'],
+        'linkage': ['ward', 'complete', 'average', 'single'],
+    },
 
-    # 'sklearn.cluster.Birch': {
-    #     'threshold': [1e-3, 1e-2, 1e-1, 1., 10., 100.],
-    #     'branching_factor': [1, 5, 10, 25, 50, 100],
-    #     'n_clusters': range(2, 21),
-    # },
+    'sklearn.cluster.Birch': {
+        'threshold': [1e-3, 1e-2, 1e-1, 1., 10., 100.],
+        'branching_factor': [2, 5, 10, 25, 50, 100],
+        'n_clusters': range(2, 21),
+    },
 
     'sklearn.cluster.DBSCAN': {
         'eps': [1e-3, 1e-2, 1e-1, 1., 10., 100.],
-        'min_samples': [3, 5, 10, 25, 50],
+        'min_samples': [1, 3, 5, 10, 25, 50],
         'metric': ['euclidean', 'l1', 'l2', 'manhattan', 'cosine', 'precomputed', 'cityblock'],
-        'leaf_size': [5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
+        'leaf_size': [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
     },
 
     'sklearn.cluster.KMeans': {
         'n_clusters': range(2, 21),
         'init': ['k-means++', 'random'],
         'algorithm': ['lloyd', 'elkan'],
-        'max_iter': np.arange(100, 600, 100),
-        'n_init': range(2,21)
+        'max_iter': np.arange(100, 600, 100)
     },
 
     'sklearn.cluster.BisectingKMeans': {
@@ -74,16 +73,16 @@ clustering_config_dict = {
 
     'sklearn.cluster.MeanShift': {
         'max_iter': np.arange(100, 600, 100),
-        'min_bin_freq': [3, 5, 10],
+        'min_bin_freq': [1, 3, 5, 10],
         'cluster_all': [True, False],
     },
 
-    # 'sklearn.cluster.OPTICS': {
-    #     'min_samples': [2, 3, 5, 10, 25, 50],
-    #     'metric': ['euclidean', 'l1', 'l2', 'manhattan', 'cosine', 'precomputed', 'cityblock'],
-    #     'cluster_method': ['xi', 'dbscan'],
-    #     'leaf_size': [1,  5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
-    # },
+    'sklearn.cluster.OPTICS': {
+        'min_samples': [2, 3, 5, 10, 25, 50],
+        'metric': ['euclidean', 'l1', 'l2', 'manhattan', 'cosine', 'precomputed', 'cityblock'],
+        'cluster_method': ['xi', 'dbscan'],
+        'leaf_size': [1,  5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
+    },
 
     'sklearn.cluster.SpectralClustering': {
         'n_clusters': range(2, 21),
@@ -100,65 +99,65 @@ clustering_config_dict = {
     #     'linkage': ['ward', 'complete', 'average'],
     # },
 
-    'sklearn.preprocessing.MaxAbsScaler': {
-    },
+#     'sklearn.preprocessing.MaxAbsScaler': {
+#     },
 
     'sklearn.preprocessing.MinMaxScaler': {
     },
 
     'sklearn.preprocessing.Normalizer': {
-        'norm': ['l1', 'l2', 'max']
+        'norm': ['l1', 'l2']
     },
 
-    'sklearn.kernel_approximation.Nystroem': {
-        'kernel': ['rbf', 'cosine', 'chi2', 'laplacian', 'polynomial', 'poly', 'linear', 'additive_chi2', 'sigmoid'],
-        'gamma': np.arange(0.0, 1.01, 0.05),
-        'n_components': range(1, 11)
-    },
+#     'sklearn.kernel_approximation.Nystroem': {
+#         'kernel': ['rbf', 'cosine', 'chi2', 'laplacian', 'polynomial', 'poly', 'linear', 'additive_chi2', 'sigmoid'],
+#         'gamma': np.arange(0.0, 1.01, 0.05),
+#         'n_components': range(1, 11)
+#     },
 
-    'sklearn.preprocessing.PolynomialFeatures': {
-        'degree': [2],
-        'include_bias': [False],
-        'interaction_only': [False]
-    },
+#     'sklearn.preprocessing.PolynomialFeatures': {
+#         'degree': [2],
+#         'include_bias': [False],
+#         'interaction_only': [False]
+#     },
 
-    'sklearn.kernel_approximation.RBFSampler': {
-        'gamma': np.arange(0.0, 1.01, 0.05)
-    },
+#     'sklearn.kernel_approximation.RBFSampler': {
+#         'gamma': np.arange(0.0, 1.01, 0.05)
+#     },
 
-    'sklearn.preprocessing.RobustScaler': {
-    },
+#     'sklearn.preprocessing.RobustScaler': {
+#     },
 
     'sklearn.preprocessing.StandardScaler': {
     },
 
-    'tpot.builtins.ZeroCount': {
-    },
+#     'tpot.builtins.ZeroCount': {
+#     },
 
-    'tpot.builtins.OneHotEncoder': {
-        'minimum_fraction': [0.05, 0.1, 0.15, 0.2, 0.25],
-        'sparse': [False],
-        'threshold': [10]
-    },
+#     'tpot.builtins.OneHotEncoder': {
+#         'minimum_fraction': [0.05, 0.1, 0.15, 0.2, 0.25],
+#         'sparse': [False],
+#         'threshold': [10]
+#     },
 
 
     # Selectors
-    'sklearn.feature_selection.SelectFwe': {
-        'alpha': np.arange(0, 0.05, 0.001),
-        'score_func': {
-            'sklearn.feature_selection.f_regression': None
-        }
-    },
+#     'sklearn.feature_selection.SelectFwe': {
+#         'alpha': np.arange(0, 0.05, 0.001),
+#         'score_func': {
+#             'sklearn.feature_selection.f_regression': None
+#         }
+#     },
 
-    'sklearn.feature_selection.SelectPercentile': {
-        'percentile': range(1, 100),
-        'score_func': {
-            'sklearn.feature_selection.f_regression': None
-        }
-    },
+#     'sklearn.feature_selection.SelectPercentile': {
+#         'percentile': range(1, 100),
+#         'score_func': {
+#             'sklearn.feature_selection.f_regression': None
+#         }
+#     },
 
     'sklearn.feature_selection.VarianceThreshold': {
-        'threshold': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.2]
+        'threshold': [0.1, 0.25]
     },
 
     'sklearn.decomposition.PCA': {
@@ -169,25 +168,25 @@ clustering_config_dict = {
         'n_components': [2, 3, 5, 10]
     },
 
-    'sklearn.feature_selection.RFE': {
-        'step': np.arange(0.05, 1.01, 0.05),
-        'estimator': {
-            'sklearn.ensemble.ExtraTreesClassifier': {
-                'n_estimators': [100],
-                'criterion': ['gini', 'entropy'],
-                'max_features': np.arange(0.05, 1.01, 0.05)
-            }
-        }
-    },
+#     'sklearn.feature_selection.RFE': {
+#         'step': np.arange(0.05, 1.01, 0.05),
+#         'estimator': {
+#             'sklearn.ensemble.ExtraTreesClassifier': {
+#                 'n_estimators': [100],
+#                 'criterion': ['gini', 'entropy'],
+#                 'max_features': np.arange(0.05, 1.01, 0.05)
+#             }
+#         }
+#     },
 
-    'sklearn.feature_selection.SelectFromModel': {
-        'threshold': np.arange(0, 1.01, 0.05),
-        'estimator': {
-            'sklearn.ensemble.ExtraTreesRegressor': {
-                'n_estimators': [100],
-                'max_features': np.arange(0.05, 1.01, 0.05)
-            }
-        }
-    }
+#     'sklearn.feature_selection.SelectFromModel': {
+#         'threshold': np.arange(0, 1.01, 0.05),
+#         'estimator': {
+#             'sklearn.ensemble.ExtraTreesRegressor': {
+#                 'n_estimators': [100],
+#                 'max_features': np.arange(0.05, 1.01, 0.05)
+#             }
+#         }
+#     }
 
 }
